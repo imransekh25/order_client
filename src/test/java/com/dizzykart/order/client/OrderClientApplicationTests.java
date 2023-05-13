b07793354995c4eb31d60f32485295afe7067740
imran <imran>
2023-05-13 07:29:39 +0530

    Added New Client

---
+++ src/test/java/com/dizzykart/order/client/OrderClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizzykart.order.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
